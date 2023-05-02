9997287479bdc09a3fe9727475bbb02f70ce1451
martin iozzi <martiniozzi120@hotmail.com>
2023-05-01 21:37:06 -0300

    tercera entrega

--- src/app.js
+++ src/app.js
@@ -5,8 +5,40 @@ const app = express() //crea un obj que tiene varios metodos
 
 let productManager = new ProductManager();
 
-app.get("/products", (req, res) =>{
-    res.send(productManager.getProducts());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/products" , async (req, res) => {     /*error que no pude encontrar: el tema es que se necesitaba poner un async await porque mis productos 
+estan puestos asincronicamente en el test() del productManager.js, por lo tanto la funcion creada en este app.js debe ser asicronica para que tome los productos del array y no sea uno vacio.*/    
+    try{
+        let products = await productManager.getProducts()
+        let type = req.query.type;
+        let limit = req.query.limit;
+        if(limit){
+            res.send(await products.slice(0, limit));
+            return;
+        }
+        if(!type || (type !== "pc" && type !== "phone")) { 
+            res.send(products)
+        } else {
+            let productsFilter = products.filter(element => element.type === type);
+            res.send (productsFilter);
+        }
+    } catch(error){
+        console.log(error)
+    }
+})
+
+app.get("/products/:pid" , async (req, res)=> {
+    try {
+        let products = await productManager.getProducts()
+        let productID = products.find((element) => {
+            return element.id == req.params.pid
+        });
+        res.send(productID);
+    } catch (e) {
+        console.log(e);
+    }
 })
 
 
--- src/productManager.js
+++ src/productManager.js
@@ -1,10 +1,9 @@
-import fs from 'fs'
-//const fs = require('fs');
+import fs from 'fs';
 
 class ProductManager {
 
     constructor() {
-        this.path = ('../info.json');
+        this.path = ('./info.json');
         this.id = 0;
         this.product = []   //Creé este array y lo pushee a la constante, creada abajo, readID. 
         //hacer un getproducts al inicio para no estar a cada rato creando una variable.
@@ -23,12 +22,12 @@ class ProductManager {
             const actualProducts = await fs.promises.readFile(this.path, 'utf-8');
             return JSON.parse(actualProducts);
         } catch (error) { 
-            console.log("No se puede agregar mas productos");
+            console.log(500, "error en el servidor");
         }
     };
 
     //Se agregan los productos, los cuales son aclarados abajo, al array de productos creado en el código de this.path.
-    async addProduct(title, description, price, thumbnail, code, stock) {
+    async addProduct(title, description, price, thumbnail, code, stock, type) {
         try {
             let product = {
                 title,
@@ -37,27 +36,23 @@ class ProductManager {
                 thumbnail,
                 code,
                 stock,
+                type,
             };
-
 
             //Creo esta variable para poder crear la id, y que, al momento de crearla, se pushee a la lista del array del JSON
             // **ACLARACIÓN: El "this.product" está creado arriba en el constructor, y se pushea mas adelante a la lista del array para poder asi, poder pushear directamente a la lista creada del array en el JSON.
             const readId =
             this.id++;
-            product.id = this.id; 
+            product.id = this.id.toString(); 
             this.product.push(product);
 
             //Esto envía la información codeada abajo al array creado en el JSON para que la lista tome el array del mismo.
             let productList = JSON.parse(await fs.promises.readFile(this.path, "utf-8"));
-                JSON.stringify(product)
                 productList.push(product);
                 await fs.promises.writeFile(this.path,
                     JSON.stringify(productList)
                 );
             //Esto verifica que el código específico del producto (ejemplo: "abc123"), no se repita mas de una vez, en tal caso, saltaría el error.
-            //const actualProductos = await this.getProducts()
-            /*if(this.product.find(element => element.code == product.code) != undefined){
-                return console.log("Ya existe el código", product.code)}*/
                 if(this.product.find(element => element.code == product.code) != undefined){
                     return console.log('Error al agregar producto: Ya existe el código "' + product.code + '"')
                 }
@@ -80,7 +75,7 @@ class ProductManager {
     //Este metodo permite leer el codigo, del array que contiene la lista de los productos.
     readCodes () {
         let readCode = JSON.parse(fs.readFileSync(this.path, 'utf-8'));
-        console.log(readCode);
+        return readCode;
     }
 
     //Este metodo, permite, seleccionando la ID del producto, poder sobreescribir los datos de uno seleccionado.
@@ -105,72 +100,156 @@ class ProductManager {
             if(index == -1){return console.log('Error al borrar producto: No existe la ID: ' + id)}
     
             actualProducts.splice(index, 1);
-            await fs.writeFileSync(this.path, JSON.stringify(actualProducts));
+            fs.writeFileSync(this.path, JSON.stringify(actualProducts));
         } catch (e) {
             console.log(e);
         }
 
     }
 }
-
 //Comienza el proceso de testeo del código.
 let listaProducto = new ProductManager();
 
 async function test (){
         /////
-    await listaProducto.addProduct(
-        "producto prueba",
-        "Este es un producto de prueba",
-        200,
-        "sin imagen",
-        "abc123",
-        25);
-    
-    await listaProducto.addProduct(
-        "producto prueba 2",
-        "Este es un producto de prueba",
-        200,
-        "sin imagen",
-        "abc456",
-        25);
+        await listaProducto.addProduct(
+            "Celular",
+            "Celular iphone 13 pro 1TB",
+            1500,
+            "sin imagen",
+            "ip13",
+            25,
+            "phone");
+        
+        await listaProducto.addProduct(
+            "Disco duro",
+            "Disco duro kingston 1TB",
+            400,
+            "sin imagen",
+            "disc123",
+            25,
+            "pc");
+        
+        await listaProducto.addProduct(
+            "Monitor",
+            `Monitor LED con 75Hz de 24 " `,
+            500,
+            "sin imagen",
+            "mon123",
+            25,
+            "pc");
 
-    await listaProducto.addProduct(
-        "producto prueba 3",
-        "Este es un producto de prueba",
-        400,
-        "sin imagen",
-        "abc112",
-        25);
-    
-    console.log("ADD listo")
-        /////
-    await listaProducto.readCodes()
-    
-    console.log("GET listo")
-        /////
-        let product = {
-            title: "producto reemplazado",
-            description: "Este es un producto reemplazado",
-            price: 300,
-            thumbnail: "sin imagen",
-            code: "abc789",
-            stock: 20
-        };
+        await listaProducto.addProduct(
+            "Teclado",
+            "Teclado mecanico HyperX",
+            300,
+            "sin imagen",
+            "keyb150",
+            25,
+            "pc"
+        )
+        await listaProducto.addProduct(
+            "Auriculares",
+            "Auriculares inalambricos HyperX con supresión de sonido",
+            200,
+            "sin imagen",
+            "auris150",
+            25,
+            "pc"
+        )
+        await listaProducto.addProduct(
+            "MousePad",
+            "MousePad negro con luces LED 80 x 25",
+            60,
+            "sin imagen",
+            "MP10",
+            25,
+            "pc"
+        )
+        await listaProducto.addProduct(
+            "Memoria Ram",
+            "Memoria Ram 16gb ddr4",
+            60,
+            "sin imagen",
+            "RaM150",
+            25,
+            "pc"
+        )
+        await listaProducto.addProduct(
+            "Procesador",
+            "Procesador AMD Ryzen 5600x",
+            750,
+            "sin imagen",
+            "MP10",
+            25,
+            "pc"
+        )
+        await listaProducto.addProduct(
+            "Lapiz Tactil",
+            "Lapiz tactil para celulares IPhone",
+            50,
+            "sin imagen",
+            "LP15",
+            25,
+            "phone"
+        )
+        await listaProducto.addProduct(
+            "AirPods",
+            "Auriculares inalambricos con cancelación de ruido",
+            250,
+            "sin imagen",
+            "AP512",
+            25,
+            "phone"
+        )
+        await listaProducto.addProduct(
+            "Cargador IPhone",
+            "Cargador IPhone carga rapida 20W",
+            210,
+            "sin imagen",
+            "CIP23",
+            25,
+            "phone"
+        )
+        await listaProducto.addProduct(
+            "Funda IPhone",
+            "Funda IPhone color rojo",
+            50,
+            "sin imagen",
+            "FD29",
+            25,
+            "phone"
+        )
+        console.log("ADD listo")
+            /////
+        listaProducto.readCodes()
+        
+        console.log("GET listo")
+            /////
+            let product = {
+                title: "Mouse",
+                description: "Mouse con sensor optico, 3200 dpi",
+                price: 100,
+                thumbnail: "sin imagen",
+                code: "M10",
+                stock: 20,
+                type: "pc"
+            };
 
     await listaProducto.updateProduct(1, product); 
     
     console.log("UPDATE listo")
         /////
-    //console.log(await listaProducto.getProductByID(1)); 
+    console.log(await listaProducto.getProductByID(1)); 
     
 
     console.log("ID recibida");
         /////
-    await listaProducto.deleteProduct(1);
-
+    //await listaProducto.deleteProduct(5);
+    
     console.log("DELETE listo");
     }
 
-test()
+//test()
 
 export default ProductManager;
